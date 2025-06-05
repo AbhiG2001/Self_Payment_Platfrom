a32e0b2bc2b59a21afc02eecd2e9b8dad73afc8b
Abhishek <drabhi2126@gmail.com>
2025-06-05 13:45:46 +0530

    corrected App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PaymentDashboard from "./Components/PaymentDashboard";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Sendmoney from "./Components/Sendmoney";
+import SendMoney from "./Components/SendMoney";
 import ScanPay from "./Components/Scanpay";
 import Offers from "./Components/Offers";
 import Recharge from "./Components/Recharge";
@@ -11,7 +11,7 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/" element={<PaymentDashboard />} />
-        <Route path="/send-money" element={<Sendmoney />} />
+        <Route path="/send-money" element={<SendMoney/>} />
         <Route path="/scan-pay" element={<ScanPay />} />
         <Route path="/recharge" element={<Recharge/>} />
         <Route path="/offers" element={<Offers />} />
